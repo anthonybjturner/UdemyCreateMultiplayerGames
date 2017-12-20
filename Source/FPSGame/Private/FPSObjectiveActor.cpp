75108c501b6f111f8162db4f89ec019f98d52fa0
Anthony Turner <anthonyturner2k@gmail.com>
2017-12-19 23:23:59 -0500

    Blackhole Challenge

--- Source/FPSGame/Private/FPSObjectiveActor.cpp
+++ Source/FPSGame/Private/FPSObjectiveActor.cpp
@@ -4,12 +4,11 @@
 #include "Components/SphereComponent.h"
 #include "Components/StaticMeshComponent.h"
 #include "Kismet/GameplayStatics.h"
+#include "FPSCharacter.h"
 
 // Sets default values
 AFPSObjectiveActor::AFPSObjectiveActor()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
 	
 	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
 	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
@@ -20,11 +19,6 @@ AFPSObjectiveActor::AFPSObjectiveActor()
 	SphereComp->SetCollisionResponseToAllChannels(ECR_Ignore);
 	SphereComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
 	SphereComp->SetupAttachment(MeshComp);
-	
-	
-
-	
-	
 }
 
 // Called when the game starts or when spawned
@@ -43,17 +37,20 @@ void AFPSObjectiveActor::PlayEffects(){
 
 }
 
-// Called every frame
-void AFPSObjectiveActor::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
 
 void AFPSObjectiveActor::NotifyActorBeginOverlap(AActor* OtherActor){
 
 	Super::NotifyActorBeginOverlap(OtherActor);
 	PlayEffects();
+
+	AFPSCharacter* MyCharacter = Cast<AFPSCharacter>(OtherActor);
+	if (MyCharacter) {
+
+		MyCharacter->bIsCarryObjective = true;
+		Destroy();
+
+		
+	}
 
 }
 
