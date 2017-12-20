e626143682f3ba1122ef7b1a47babfa190e96378
Anthony Turner <anthonyturner2k@gmail.com>
2017-12-20 00:35:43 -0500

    Added Extraction feedback.

---
+++ Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPSExtractionZone.h"
+#include "Components/BoxComponent.h"
+#include "Components/DecalComponent.h"
+#include "FPSCharacter.h"
+#include "FPSGameMode.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AFPSExtractionZone::AFPSExtractionZone()
+{
+
+	OverlapComp = CreateDefaultSubobject<UBoxComponent>(TEXT("OverlapComp"));
+	OverlapComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	OverlapComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	OverlapComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	OverlapComp->SetBoxExtent(FVector(200.f));
+	RootComponent = OverlapComp;
+
+	OverlapComp->OnComponentBeginOverlap.AddDynamic(this, &AFPSExtractionZone::HandleOverlap);
+	OverlapComp->SetHiddenInGame(false);
+
+	DecalComp = CreateDefaultSubobject<UDecalComponent>(TEXT("DecalComp"));
+	DecalComp->DecalSize = FVector(200.f);
+	DecalComp->SetupAttachment(OverlapComp);
+}
+
+
+void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult) {
+
+
+	AFPSCharacter* MyCharacter = Cast<AFPSCharacter>(OtherActor);
+	if (MyCharacter == nullptr) { return; }
+
+	if (MyCharacter->bIsCarryObjective) {
+
+		AFPSGameMode* GM = Cast<AFPSGameMode>(GetWorld()->GetAuthGameMode());
+		if (GM) {
+
+			GM->CompleteMission(MyCharacter);
+		}
+	} else {//Play don't have objective sound
+
+		UGameplayStatics::PlaySound2D(this, ObjectiveMissingSound);
+	}
+
+
+}
+
